4d9dcb3ca619c90bd7a094c5befbc79c9a03cec0
T4ZERx <thomassauvageot@outlook.com>
2023-11-10 16:53:32 +0100

    Ajout réalisation

--- src/components/Realisations.vue
+++ src/components/Realisations.vue
@@ -3,36 +3,46 @@
     <h1 class="titleComponents">Réalisations</h1>
   </div>
   <div class="row">
+    <div class="col-md-6 col-sm-12 cardMobile">
+      <q-card class="q-ma-sm">
+        <q-img src="~assets/essert.png" style="height: 544px">
+          <div class="absolute-bottom">
+            <div class="text-h6">Mairie d'Essert</div>
+            <div class="text-subtitle2">Site vitrine</div>
+          </div>
+        </q-img>
 
-  <div class="col-md-6 col-sm-12 cardMobile">
-  <q-card  class="q-ma-sm ">
-    <q-img src="https://cdn.quasar.dev/img/parallax2.jpg">
-      <div class="absolute-bottom">
-        <div class="text-h6">Mairie d'Essert</div>
-        <div class="text-subtitle2">Site vitrine</div>
-      </div>
-    </q-img>
+        <q-card-actions>
+          <q-btn flat>Visiter le site</q-btn>
+        </q-card-actions>
+      </q-card>
+    </div>
 
-    <q-card-actions>
-      <q-btn flat>Visiter le site</q-btn>
-    </q-card-actions>
-  </q-card>
-  </div>
+    <div class="col-md-6 col-sm-12 cardMobile">
+      <q-card class="q-ma-sm">
+        <q-img src="~assets/loading.png" style="height: 544px">
+          <div class="absolute-bottom">
+            <div class="text-h6">Mairie de Frahier-et-Chatebier</div>
+            <div class="text-subtitle2">Site vitrine</div>
+          </div>
+        </q-img>
 
-  <div class="col-md-6 col-sm-12 cardMobile">
-  <q-card class="q-ma-sm ">
-    <q-img src="https://cdn.quasar.dev/img/parallax2.jpg">
-      <div class="absolute-bottom">
-        <div class="text-h6">Mairie de Frahier-et-Chatebier</div>
-        <div class="text-subtitle2">Site vitrine</div>
-      </div>
-    </q-img>
+        <q-card-actions>
+          <q-btn flat>Visiter le site</q-btn>
+        </q-card-actions>
+      </q-card>
+    </div>
 
-    <q-card-actions>
-      <q-btn flat>Visiter le site</q-btn>
-    </q-card-actions>
-  </q-card>
-  </div>
+    <div class="col-md-6 col-sm-12 cardMobile">
+      <q-card class="q-ma-sm">
+        <q-img src="~assets/loading.png" style="height: 544px">
+          <div class="absolute-bottom">
+            <div class="text-h6">Pole jeunesse Essert</div>
+            <div class="text-subtitle2">Maintenance du site internet</div>
+          </div>
+        </q-img>
+      </q-card>
+    </div>
   </div>
 </template>
 <script>
